34f28810d25ce6de0d57b57fc806c3b05eb372a9
TanujDas7 <tanujdas.unoff@gmail.com>
2023-03-10 20:46:13 +0530

    scripted pipeline with stages and steps

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,17 +2,23 @@ pipeline{
     agent { node { label "${env.AGENT_LABEL}"}}
     stages{
         stage('code checkout') {
-                    echo "git clone"
+            steps{
+                echo "git clone"
                     git branch: 'master', credentialsId: 'd9571958-53a3-4fc3-b83a-d9807504e93d', url: 'https://github.com/TanujDas7/job.git'
+            }
                 }
             stage("package"){
+                steps{
                     echo "html file content"
                         echo "$pwd"
                         echo "$WORKSPACE"
                     }
+                }
             stage("ec2"){
-                    echo "write into ec2"
+                steps{
+                     echo "write into ec2"
                     // sh "sh scripts.sh"
                 }
+            }
     }
 }
