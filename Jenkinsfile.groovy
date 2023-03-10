63960f8cb8715d1f75c00b59bb08e1b77fad64b3
TanujDas7 <tanujdas.unoff@gmail.com>
2023-03-10 22:08:54 +0530

    zip and unzip

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,3 +1,7 @@
+import java.nio.file.Files
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+
 pipeline{
     agent { node { label "${env.AGENT_LABEL}"}}
     stages{
@@ -9,15 +13,67 @@ pipeline{
                 }
             stage("package"){
                 steps{
-                    echo "html file content"
-                        echo "$pwd"
+                        echo "zip pem and html"
                         echo "$WORKSPACE"
+                        sh "mkdir utils"
+                        sh "mv home.html utils"
+                        sh "mv mumbai.pem utils"
+                        String zipFileName = "utils.zip"
+                        String inputDir = "utils"
+
+                        ZipOutputStream output = new ZipOutputStream(new FileOutputStream(zipFileName))
+                        new File(inputDir).eachFile() { file ->
+                            if (!file.isFile()) {
+                                return
+                            }
+
+                            output.putNextEntry(new ZipEntry(file.name.toString())) // Create the name of the entry in the ZIP
+
+                            InputStream input = new FileInputStream(file);
+
+                            // Stream the document data to the ZIP
+                            Files.copy(input, output);
+                            output.closeEntry(); // End of current document in ZIP
+                            input.close()
+                        }
+                        output.close();
+
                     }
                 }
             stage("ec2"){
                 steps{
-                     echo "write into ec2"
-                    // sh "sh scripts.sh"
+                    echo "write into ec2"
+                    String zipFileName = "utils.zip"  
+                    def outputDir = "zip"
+                    byte[] buffer = new byte[1024]
+                    ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFileName))
+                    ZipEntry zipEntry = zis.getNextEntry()
+                    while (zipEntry != null) {
+                        File newFile = new File(outputDir+ File.separator, zipEntry.name)
+                        if (zipEntry.isDirectory()) {
+                            if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                                throw new IOException("Failed to create directory " + newFile)
+                            }
+                        } else {
+                            // fix for Windows-created archives
+                            File parent = newFile.parentFile
+                            if (!parent.isDirectory() && !parent.mkdirs()) {
+                                throw new IOException("Failed to create directory " + parent)
+                            }
+                            // write file content
+                            FileOutputStream fos = new FileOutputStream(newFile)
+                            int len = 0
+                            while ((len = zis.read(buffer)) > 0) {
+                                fos.write(buffer, 0, len)
+                            }
+                            fos.close()
+                        }
+                    zipEntry = zis.getNextEntry()
+                    }
+                    zis.closeEntry()
+                    zis.close()
+
+                    sh "ls -al"
                 }
             }
     }
