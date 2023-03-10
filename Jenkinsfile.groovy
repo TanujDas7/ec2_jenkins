0492c9ca1a02b229fcb46c4c36ff90b37758d826
TanujDas7 <tanujdas.unoff@gmail.com>
2023-03-10 20:37:59 +0530

    ref

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,8 @@
-agent = "${env.AGENT_LABEL}";
-
 pipeline{
-    agent{ node{label "${agent}"}}
-        stages{
-            stage("code checkout"){
-                steps{
+    agent { node { label "${env.AGENT_LABEL}"}}
+     stages{
+        stage('code checkout') {
+            steps {
                     echo "git clone"
                     git branch: 'master', credentialsId: 'd9571958-53a3-4fc3-b83a-d9807504e93d', url: 'https://github.com/TanujDas7/job.git'
                 }
