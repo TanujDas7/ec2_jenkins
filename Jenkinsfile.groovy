a73ff6902ed1f47662e82c6d6fe796ab4cd36efc
TanujDas7 <tanujdas.unoff@gmail.com>
2023-03-10 22:39:30 +0530

    no zips

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,3 @@
-import java.nio.file.Files
-import java.util.zip.*
-
 pipeline{
     agent { node { label "${env.AGENT_LABEL}"}}
     stages{
@@ -15,73 +12,21 @@ pipeline{
                         script{echo "zip pem and html"
                         sh "mkdir -p utils"
                         sh "mv home.html utils"
-                        sh "mv mumbai.pem utils"}
-                        zipper()
-                        script{
-                            sh "cd utils"
-                            sh "ls"
-                            sh "cd .."
-                        }
-                        
+                        sh "mv mumbai.pem utils"}  
                     }
                 }
             stage("ec2"){
                 steps{
-                    unzipper()
+                    echo "unzip files from utils"
                     script{
-                        sh "ls -al"
+                        sh "curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip"
+                        sh "sudo .//aws//install"
+                        sh "aws configure --help"
+                        // sh "chmod 400 mumbai.pem"
+                        // sh "ssh -i mumbai.pem ec2-user@"
                     }
                 }
             }
     }
-}
-
-def zipper(){
-    String zipFileName = "utils.zip"
-    String inputDir = "utils"
-    ZipOutputStream zipFile = new ZipOutputStream(new FileOutputStream(zipFileName))  
-    new File(inputDir).eachFile() { file -> 
-    //check if file
-    if (file.isFile()){
-        zipFile.putNextEntry(new ZipEntry(file.name))
-        def buffer = new byte[file.size()]  
-        file.withInputStream { 
-        zipFile.write(buffer, 0, it.read(buffer))  
-        }  
-        zipFile.closeEntry()
-    }
-    }  
-    zipFile.close()  
-}
-
-def unzipper(){
-    String zipFileName = "utils.zip"  
-    def outputDir = "zip"
-    byte[] buffer = new byte[1024]
-    ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFileName))
-    ZipEntry zipEntry = zis.getNextEntry()
-    while (zipEntry != null) {
-        File newFile = new File(outputDir+ File.separator, zipEntry.name)
-        if (zipEntry.isDirectory()) {
-            if (!newFile.isDirectory() && !newFile.mkdirs()) {
-                throw new IOException("Failed to create directory " + newFile)
-            }
-        } else {
-            // fix for Windows-created archives
-            File parent = newFile.parentFile
-            if (!parent.isDirectory() && !parent.mkdirs()) {
-                throw new IOException("Failed to create directory " + parent)
-            }
-            // write file content
-            FileOutputStream fos = new FileOutputStream(newFile)
-            int len = 0
-            while ((len = zis.read(buffer)) > 0) {
-                fos.write(buffer, 0, len)
-            }
-            fos.close()
-        }
-    zipEntry = zis.getNextEntry()
-    }
-    zis.closeEntry()
-    zis.close()
 }
