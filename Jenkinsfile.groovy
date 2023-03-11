d43583a5364d4d29aeb2dd8de5a3dc92d1b53e0e
TanujDas7 <tanujdas.unoff@gmail.com>
2023-03-11 19:30:11 +0530

    one step

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,15 +17,15 @@ pipeline{
                 }
             stage("ec2"){
                 steps{
-                    echo "unzip files from utils"
-                        step{
-                            script{
+                    step{
+                        echo "unzip files from utils"
+                        script{
                             sh "cd utils"
                             sh "ls -al"
                             // sh "chmod 400 mumbai.pem"
                             // sh "ssh -i mumbai.pem ec2-user@52.66.210.187"
                         }
-                        }
+                    }
                 }
             }
     }
