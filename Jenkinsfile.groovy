3e32c614d228ca0113a043f81357cb0469657f26
TanujDas7 <tanujdas.unoff@gmail.com>
2023-03-13 00:56:51 +0530

    -o StrictHostKeyChecking=no for ssh

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -37,7 +37,6 @@ pipeline{
                         sh script:'''
                         cd utils
                         mv home.html index.html
-                        chmod 400 chmod.pem
                         ssh -o StrictHostKeyChecking=no ec2-user@13.234.66.18
                         scp index.html ec2-user@13.234.66.18:/var/www/html
                         '''
