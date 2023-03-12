61c905ed6b9738341ff5d037234624f5fc34a2aa
TanujDas7 <tanujdas.unoff@gmail.com>
2023-03-12 23:59:53 +0530

    comment

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -36,9 +36,9 @@ pipeline{
                     sshagent(credentials : ['3.109.59.247']) {
                         sh script:'''
                         ssh -o StrictHostKeyChecking=no ec2-user@3.109.59.247 uptime
-                        // ssh -v ec2-user@3.109.59.247
                         scp index.html ec2-user@3.109.59.247:/var/www/html/
                         '''
+                        // ssh -v ec2-user@3.109.59.247
                     }
                 }
             }
