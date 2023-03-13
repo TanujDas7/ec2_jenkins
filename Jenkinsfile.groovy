89f49efa8768d7491eb9e86beb7192e40f70784b
TanujDas7 <tanujdas.unoff@gmail.com>
2023-03-13 14:02:48 +0530

    ssh -T

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -34,7 +34,7 @@ pipeline{
 
                         ssh -o StrictHostKeyChecking=no ec2-user@65.2.4.132 '
                         cd /var/www/html
-                        ls
+                        unzip utils.zip -d .
                         '
 
                         '''
